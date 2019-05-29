b4879343722bba7315f7a0b49ba7bb6f79b4602a
rsyong <409431858@qq.com>
2019-05-29 23:18:09 +0800

    19.5.29.2

---
+++ js/common.js
@@ -0,0 +1,50 @@
+(function () {
+    //适配移动端rem
+    var newRem = function () {
+        var html = document.documentElement;
+        var ClientWidth = html.getBoundingClientRect().width;
+        html.style.fontSize = ClientWidth / 108 + 'px';
+    };
+    window.addEventListener('resize', newRem, false);
+    newRem();
+    
+    
+    function flashChecker() {
+	        var hasFlash = 0;         //是否安装了flash
+	        var flashVersion = 0; //flash版本
+	        var isIE = /*@cc_on!@*/0;      //是否IE浏览器
+	
+	        if (isIE) {
+	            var swf = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
+	            if (swf) {
+	                hasFlash = 1;
+	                VSwf = swf.GetVariable("$version");
+	                flashVersion = parseInt(VSwf.split(" ")[1].split(",")[0]);
+	            }
+	        } else {
+	            if (navigator.plugins && navigator.plugins.length > 0) {
+	                var swf = navigator.plugins["Shockwave Flash"];
+	                if (swf) {
+	                    hasFlash = 1;
+	                    var words = swf.description.split(" ");
+	                    for (var i = 0; i < words.length; ++i) {
+	                        if (isNaN(parseInt(words[i]))) continue;
+	                        flashVersion = parseInt(words[i]);
+	                    }
+	                }
+	            }
+	        }
+	        return {f: hasFlash, v: flashVersion};
+	    }
+	
+	    var fls = flashChecker();
+	    var s = "";
+	    if (fls.f) {
+	    	$(".openFlash").hide();
+	//        document.write("您安装了flash,当前flash版本为: " + fls.v + ".x");
+	    }
+	    else {
+	    	$(".openFlash").show();
+	//        document.write("您没有安装flash");
+	    }
+})();
